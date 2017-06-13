43d6608459b673f45318206f7a7c7f15e43d8164
Ian Woodbury <sparkworx@gmail.com>
2017-06-12 22:59:26 -0700

    Add support for RPM-YUM-ish Linux distributions (CentOS & Oracle Linux)
    `update.sh` logic is a tad kludgish, but BuildPack was originally
    intended for Debian distros; Oracle's Docker tags also present a slight
    challenge.

--- update.sh
+++ update.sh
@@ -11,6 +11,8 @@ versions=( "${versions[@]%/}" )
 
 debian="$(curl -fsSL 'https://raw.githubusercontent.com/docker-library/official-images/master/library/debian')"
 ubuntu="$(curl -fsSL 'https://raw.githubusercontent.com/docker-library/official-images/master/library/ubuntu')"
+centos="$(curl -fsSL 'https://raw.githubusercontent.com/docker-library/official-images/master/library/centos' )"
+oraclelinux="oraclelinux7 oraclelinux7-slim" # hard-coded hack, similar to "centos{digit}"
 
 travisEnv=
 for version in "${versions[@]}"; do
@@ -18,18 +20,41 @@ for version in "${versions[@]}"; do
 		dist='debian'
 	elif echo "$ubuntu" | grep -qE "\b$version\b"; then
 		dist='ubuntu'
+	elif echo "$centos" | grep -qE "\b$version\b"; then
+		dist='centos'
+	elif echo "$oraclelinux" | grep -qE "\b$version\b"; then
+		dist='oraclelinux'
 	else
 		echo >&2 "error: cannot determine repo for '$version'"
 		exit 1
 	fi
 	for variant in curl scm ''; do
-		src="Dockerfile${variant:+-$variant}.template"
-		trg="$version${variant:+/$variant}/Dockerfile"
+		case $dist in
+			centos|oraclelinux)
+				# rpm-yum-ish distros
+				src="Dockerfile${variant:+-$variant}-yum.template"
+				trg="$version${variant:+/$variant}/Dockerfile"
+				;;
+			*)
+				# deb-apt-ish distros
+				src="Dockerfile${variant:+-$variant}.template"
+				trg="$version${variant:+/$variant}/Dockerfile"
+				;;
+		esac
 		mkdir -p "$(dirname "$trg")"
+		if [[ $variant == curl && $dist == oraclelinux ]] ; then
+    # Oracle Linux uses {digit} versions only,
+    # so customize tag on "curl" variant.
+		( set -x && sed '
+			s!DIST!'"$dist"'!g;
+			s!SUITE!'"${version##${dist}}"'!g;
+		' "$src" > "$trg" )
+		else
 		( set -x && sed '
 			s!DIST!'"$dist"'!g;
 			s!SUITE!'"$version"'!g;
 		' "$src" > "$trg" )
+		fi
 	done
 	travisEnv+='\n  - VERSION='"$version"
 done
